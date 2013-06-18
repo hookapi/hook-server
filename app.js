62d267417be4da0c32a55907db31797ff6c9ac03
Gregory Koberger <gkoberger@gmail.com>
2013-06-18 16:10:02 -0700

    Update docs for app

--- app.js
+++ app.js
@@ -189,8 +189,7 @@ app.post('/user', user.post);
 /*
  * Log in 
  * @Public: false, only with authentication
- * @TODO: Update this with the correct info
- * @raw: curl -X POST -u "testuser:123" -d "password=test&rsakey=1234567" http://localhost:4001/user/login
+ * @raw: curl -X POST -d "username=testuser&password=123" http://localhost:4001/user/login
  */
 
 app.post('/user/login', auth, user.login);
